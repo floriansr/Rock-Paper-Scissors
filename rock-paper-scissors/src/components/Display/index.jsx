7618ca35f022a92fbfd147480b7519d50e8e04cc
floriansr <sueurfloriansf@gmail.com>
2020-07-04 18:28:12 +0200

    [add] set current choice for all players

--- rock-paper-scissors/src/components/Display/index.jsx
+++ rock-paper-scissors/src/components/Display/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Button } from 'antd';
 
 const Display = () => {
+  const [choice, setChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+
+  const setCurrentChoice = (e) => {
+    // console.log('setChoice -> choice', e.target.textContent);
+    setChoice(e.target.textContent);
+
+    const allChoices = ['paper', 'rock', 'scissors'];
+    const computer = allChoices[Math.floor(Math.random() * allChoices.length)];
+    // console.log('setCurrentChoice -> computer', computer);
+    setComputerChoice(computer);
+  };
+
   return (
     <>
-      <h3>This is the display</h3>
+      <Button type="primary" onClick={(e) => setCurrentChoice(e)}>
+        Rock
+      </Button>
+      <Button type="primary" onClick={(e) => setCurrentChoice(e)}>
+        Paper
+      </Button>
+      <Button type="primary" onClick={(e) => setCurrentChoice(e)}>
+        Scissors
+      </Button>
+
+      {choice ? (
+        <div>
+          <h3>My current choice : {choice}</h3>
+          <h3>Computer choice : {computerChoice}</h3>
+        </div>
+      ) : (
+        ''
+      )}
     </>
   );
 };
