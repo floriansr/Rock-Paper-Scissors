d68ef3b4e2be45c6b9604026c7c4cf72fce8ca38
floriansr <sueurfloriansf@gmail.com>
2020-07-05 15:43:32 +0200

    [add] Ending Game component with reset function

---
+++ rock-paper-scissors/src/tools/EndingGame/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { Result, Button } from 'antd';
+
+import { removeChoice, removeScore } from '../../redux';
+
+const EndingGame = () => {
+  const { scorePlayer1, scorePlayer2 } = useSelector((state) => state.score);
+  const dispatch = useDispatch();
+
+  const newGame = () => {
+    dispatch(removeChoice());
+    dispatch(removeScore());
+  };
+
+  return (
+    <>
+      <Result
+        status={scorePlayer1 > scorePlayer2 ? 'success' : 'error'}
+        title={
+          scorePlayer1 > scorePlayer2
+            ? 'Player 1 win this game'
+            : 'Player 2 win this game'
+        }
+        subTitle={`Player 1 : ${scorePlayer1} / Player 2 : ${scorePlayer2}`}
+        extra={[
+          <Button type="primary" key="newgame" onClick={newGame}>
+            New Game
+          </Button>,
+        ]}
+      />
+    </>
+  );
+};
+
+export default EndingGame;
