49c780524b38f298b92eecb2be9bb3bd2c307c3a
floriansr <sueurfloriansf@gmail.com>
2020-07-05 14:29:40 +0200

    [add] compare choices

--- rock-paper-scissors/src/components/Display/index.jsx
+++ rock-paper-scissors/src/components/Display/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 
 import { setChoice } from '../../redux';
 
@@ -9,10 +9,37 @@ const Display = () => {
   const dispatch = useDispatch();
   const { choicePlayer1, choicePlayer2 } = useSelector((state) => state.choice);
 
+  const compareChoice = (player1, player2) => {
+    // DRAW
+    if (player1 === player2) return message.warning('This is a draw');
+
+    // PLAYER 1 WIN
+    if (player1 === 'Scissors' && player2 === 'Paper')
+      return message.success('Player 1 win !', 3);
+
+    if (player1 === 'Rock' && player2 === 'Scissors')
+      return message.success('Player 1 win !', 3);
+
+    if (player1 === 'Paper' && player2 === 'Rock')
+      return message.success('Player 1 win !', 3);
+
+    // PLAYER 2 WIN
+
+    if (player2 === 'Paper' && player1 === 'Rock')
+      return message.error('Player 2 win !', 3);
+
+    if (player2 === 'Rock' && player1 === 'Scissors')
+      return message.error('Player 2 win !', 3);
+
+    if (player2 === 'Scissors' && player1 === 'Paper')
+      return message.error('Player 2 win !', 3);
+  };
+
   const setCurrentChoice = (e) => {
-    const allChoices = ['Paper', 'Rock', 'Scissors'];
+    const allChoices = ['Rock', 'Paper', 'Scissors'];
     const computer = allChoices[Math.floor(Math.random() * allChoices.length)];
     dispatch(setChoice(e.target.textContent, computer));
+    compareChoice(e.target.textContent, computer);
   };
 
   return (
